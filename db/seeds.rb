9822be31fbf01276ce92072568d644c5a5bffdc3
Robert Johnston <robjnavy@gmail.com>
2016-01-25 20:57:34 +0000

    Checkpoint 40 Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -69,9 +69,18 @@
    )
  end
  
- user = User.first
- user.update_attributes!(
-   email: 'robert.seth.johnston@gmail.com', # replace this with your personal email
+ # Create an admin user
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ 
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
    password: 'helloworld'
  )
  
