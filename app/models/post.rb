e9da2d6feae7816e853410338575ddeb7b629b06
Robert Johnston <robjnavy@gmail.com>
2016-02-03 21:02:10 +0000

    Assignment 43 Complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,7 @@ class Post < ActiveRecord::Base
     validates :body, length: { minimum: 20 }, presence: true
     validates :topic, presence: true
     validates :user, presence: true
+    after_create :create_vote
     
     
     def up_votes
@@ -31,5 +32,11 @@ class Post < ActiveRecord::Base
      age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
+   end
+   
+   private
+   
+   def create_vote
+       user.votes.create(value: 1, post: self)
    end
 end
