679c2648a35afa99f554547282e504676d0c9296
Robert Johnston <robjnavy@gmail.com>
2016-02-16 22:54:23 +0000

    Assignment 47 Complete

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   validates :body, length: { minimum: 5 }, presence: true
   validates :user, presence: true
-  default_scope { order('updated_at DESC') }
+  default_scope { order('updated_at ASC') }
   
   after_create :send_favorite_emails
  
